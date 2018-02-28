eede00259da97eadca554ac5c90a00230ce45c2c
Bas Janssen <smh.janssen@gmail.com>
2018-02-28 22:29:40 +0100

    Added admin area to the app. Locked overview from normal users.

--- photovote.py
+++ photovote.py
@@ -8,10 +8,12 @@
 #@author: Bas Janssen
 #"""
 
-from flask import Flask, render_template, request, session, Markup
+from flask import Flask, render_template, request, session, Markup, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import uuid
 from datetime import timedelta
+import getpass
 
 
 app = Flask(__name__)
@@ -49,27 +51,42 @@ def index():
         _script = _script + "$('{_photographer}').starRating({{starSize: 25, initialRating: {_rating}, disableAfterRate: false, callback: function(currentRating, $el){{$.post('addRating', {{'id': $el[0].id, 'rating': currentRating}});}}}});".format(_photographer = ".photo-rating-" + str(row[0]), _rating = _currentRating)
     _overview = _overview + "</table>"
     _script = _script + "});</script>"
-    return render_template('index.html', overview = Markup(_overview), script=Markup(_script))
+    _navbar = "<nav><ul class='nav nav-pills float-right'><li class='nav-item'><a class='nav-link active' href='/login'>Login</a></li></ul></nav>"
+    return render_template('index.html', navbar = Markup(_navbar), overview = Markup(_overview), script=Markup(_script))
 
 @app.route('/overview')
 def overview():
-    try:
-        _photographers = conn.execute('''select ID, NAME from Photographers;''')
-    except sqlite3.Error as e:
-        return render_template('error.html', error = str(e.args[0]))
-    _overview = "<table class='table table-hover'><tr><th>Photographers</th></tr>"
-    _script = "<script>$(document).ready( function() {"
-    for row in _photographers:
-        try:
-            _rating = conn.execute('''select avg(RATING), sum(RATING) from Ratings where PHOTOGRAPHER={_Photographer} and DAY=date('now');'''.format(_Photographer=row[0]))
-        except sqlite3.Error as e:
-            return render_template('error.html', error = str(e.args[0]))
-        row2 = _rating.fetchone()
-        _overview = _overview + "<tr><td data-toggle='collapse' data-target='#{_photographer}' class='clickable'>{_photographer}</td><td>{_TotalScore}</td><td><div id='{_photographer}' class='photo-rating-{_photographer}'></div></td></tr>".format(_photographer = row[0], _TotalScore = row2[1] or 0)
-        _script = _script + "$('{_photographer}').starRating({{starSize: 25, readOnly: true, initialRating: {_rating}}});".format(_photographer = ".photo-rating-" + str(row[0]), _rating = row2[0] or 0)
-    _overview = _overview + "</table>"
-    _script = _script + "});</script>"
-    return render_template('index.html', overview = Markup(_overview), script=Markup(_script))
+    if session.get('uuid'):
+        if session.get('user'):
+            try:
+                _admins = conn.execute("select ID from Admin where NAME='{_Username}' and UUID='{_uuid}';".format(_Username = session.get('user'), _uuid = session.get('uuid')))
+            except sqlite3.Error as e:
+                return render_template('error.html', error = str(e.args[0]))
+            if _admins.fetchone() is None:
+                return redirect('/')
+            else:
+                try:
+                    _photographers = conn.execute('''select ID, NAME from Photographers;''')
+                except sqlite3.Error as e:
+                    return render_template('error.html', error = str(e.args[0]))
+                _overview = "<table class='table table-hover'><tr><th>Photographers</th></tr>"
+                _script = "<script>$(document).ready( function() {"
+                for row in _photographers:
+                    try:
+                        _rating = conn.execute('''select avg(RATING), sum(RATING) from Ratings where PHOTOGRAPHER={_Photographer} and DAY=date('now');'''.format(_Photographer=row[0]))
+                    except sqlite3.Error as e:
+                        return render_template('error.html', error = str(e.args[0]))
+                    row2 = _rating.fetchone()
+                    _overview = _overview + "<tr><td data-toggle='collapse' data-target='#{_photographer}' class='clickable'>{_photographer}</td><td>{_TotalScore}</td><td><div id='{_photographer}' class='photo-rating-{_photographer}'></div></td></tr>".format(_photographer = row[0], _TotalScore = row2[1] or 0)
+                    _script = _script + "$('{_photographer}').starRating({{starSize: 25, readOnly: true, initialRating: {_rating}}});".format(_photographer = ".photo-rating-" + str(row[0]), _rating = row2[0] or 0)
+                _overview = _overview + "</table>"
+                _script = _script + "});</script>"
+                _navbar = "<nav><ul class='nav nav-pills float-right'><li class='nav-item'><a class='nav-link active' href='/logout'>Logout</a></li><li class='nav-item'><a class='nav-link' href='/add_photographer'>Add Photographer</a></li><li class='nav-item'><a class='nav-link' href='/add_admin'>Add Admin</a></li></ul></nav>"
+                return render_template('index.html', navbar = Markup(_navbar), overview = Markup(_overview), script=Markup(_script))
+        else:
+            return redirect('/')
+    else:
+        return redirect('/')
 
 @app.route("/addRating", methods=['POST'])
 def addRating():
@@ -84,12 +101,120 @@ def addRating():
     else:
         return "invalid"
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        try:
+            _username = request.form['inputUsername']
+            _password = request.form['inputPassword']
+        except Exception as e:
+            return render_template('error.html', error = str(e))
+        
+        try:
+            _pw_hashes = conn.execute("select PASSWORDHASH, UUID from Admin where NAME='{_user}';".format(_user = _username))
+            for _pw_hash in _pw_hashes:
+                pw_hash = _pw_hash[0]
+                uuid = _pw_hash[1]
+        except sqlite3.Error as e:
+            return render_template('error.html', error = str(e.args[0]))
+        if check_password_hash(pw_hash, _password):
+            session['user'] = _username
+            session['uuid'] = uuid
+            return redirect('/overview')
+        else:
+            return render_template('error.html',error = 'Invalid password.')
+    
+    return render_template("login.html", path=Markup("login"), action=Markup("Login"))
+    
+@app.route("/logout")
+def logout():
+    session.pop('user', None)
+    return redirect('/')
+
+@app.route("/add_photographer", methods=['GET', 'POST'])
+def add_photographer():
+    if session.get('uuid'):
+        if session.get('user'):
+            try:
+                _admins = conn.execute("select ID from Admin where NAME='{_Username}' and UUID='{_uuid}';".format(_Username = session.get('user'), _uuid = session.get('uuid')))
+            except sqlite3.Error as e:
+                return render_template('error.html', error = str(e.args[0]))
+            if _admins.fetchone() is None:
+                return redirect('/')
+            else:
+                if request.method == 'POST':
+                    try:
+                        photographer = request.form['inputPhotographer']
+                    except Exception as e:
+                        return render_template('error.html', error = str(e))
+                    try:
+                        conn.execute("insert into Photographers (NAME) values ('{_photographer}');".format(_photographer = photographer))
+                        conn.commit()
+                    except sqlite3.Error as e:
+                        return render_template('error.html', error = str(e.args[0]))
+                    return redirect('/overview')
+                    
+                return render_template('add_photographer.html')
+        else:
+            return redirect('/')
+    else:
+        return redirect('/')
+
+@app.route("/add_admin", methods=['GET', 'POST'])
+def add_admin():
+    if session.get('uuid'):
+        if session.get('user'):
+            try:
+                _admins = conn.execute("select ID from Admin where NAME='{_Username}' and UUID='{_uuid}';".format(_Username = session.get('user'), _uuid = session.get('uuid')))
+            except sqlite3.Error as e:
+                return render_template('error.html', error = str(e.args[0]))
+            if _admins.fetchone() is None:
+                return redirect('/')
+            else:
+                if request.method == 'POST':
+                    try:
+                        _username = request.form['inputUsername']
+                        _password = request.form['inputPassword']
+                    except Exception as e:
+                        return render_template('error.html', error = str(e))
+                    try:
+                        conn.execute("insert into Admin (UUID, NAME, PASSWORDHASH) values ('{_UUID}', '{_Name}', '{_PWHash}');".format(_UUID = uuid.uuid4(), _Name = _username, _PWHash = generate_password_hash(_password)))
+                        conn.commit()
+                    except sqlite3.Error as e:
+                        return render_template('error.html', error = str(e.args[0]))
+                    return redirect('/overview')
+                    
+                return render_template("login.html", path=Markup("add_admin"), action=Markup("Add Admin"))
+        else:
+            return redirect('/')
+    else:
+        return redirect('/')
+
+
 if __name__=="__main__":
     conn = sqlite3.connect('photovote.db')
     conn.execute('''PRAGMA foreign_keys = ON;''')
-    conn.execute('''create table if not exists Admin(ID INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, NAME TEXT NOT NULL, PASSWORDHASH TEXT NOT NULL);''') #The password must be hashed, plaintext can not be used.
+    conn.execute('''create table if not exists Admin(ID INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, UUID TEXT, NAME TEXT, PASSWORDHASH TEXT);''') #The password must be hashed, plaintext can not be used.
     conn.execute('''create table if not exists Photographers(ID INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, NAME TEXT NOT NULL);''')
     conn.execute('''create table if not exists Ratings(ID INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, RATING REAL NOT NULL, DAY TEXT NOT NULL, USER TEXT NOT NULL, PHOTOGRAPHER INT NOT NULL, FOREIGN KEY(PHOTOGRAPHER) REFERENCES PHOTOGRAPHERS(ID));''')
+    
+    try:
+        _admins = conn.execute("select ID from Admin;")
+    except sqlite3.Error as e:
+        print (str(e.args[0]))
+    if _admins.fetchone() is None:
+        print ("No admins found on startup. Please add an admin using the following promts.")
+        username = raw_input("Username: ")
+        password = getpass.getpass("Password: ")
+        try:
+            conn.execute("insert into Admin (UUID, NAME, PASSWORDHASH) values ('{_UUID}', '{_Name}', '{_PWHash}');".format(_UUID = uuid.uuid4(), _Name = username, _PWHash = generate_password_hash(password)))
+            conn.commit()
+        except sqlite3.Error as e:
+            print (str(e.args[0]))
+        print("Thank you, Admin has been added.")
+        username = None
+        password = None
+        
     app.run(host='127.0.0.1', port=8000)
     
     while True:

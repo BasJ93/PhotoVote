8c27a303e56342bfdfabd0fff7aaebbd67c05743
Bas Janssen <smh.janssen@gmail.com>
2018-05-30 21:32:53 +0200

    Forgot to add the new files.

---
+++ static/sw.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'PhotoVote-cache-v1';
+	var urlsToCache = [
+		'/',
+		'/index.css',
+		'/jquery-star-rating-svg.js',
+		'/star-rating-svg.css',
+		'/login',
+		'/overview'
+		];
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			})
+		);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+			.then(function(response) {
+				if (response) {
+					return response;
+				}
+				return fetch(event.request);
+			}
+		)
+	);
+});
